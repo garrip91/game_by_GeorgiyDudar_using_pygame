1846e3cce7c0e08d6e755dfefec70426f891bc47
garrip91 <garrip91@gmail.com>
2023-12-10 17:59:21 +0300

    Adding animations and sounds

--- src/main.py
+++ src/main.py
@@ -1,27 +1,54 @@
 import pygame
 
 
+clock = pygame.time.Clock()
+
 pygame.init()
-screen = pygame.display.set_mode((600, 300)) # , flags=pygame.NOFRAME
+screen = pygame.display.set_mode((1280, 748)) # , flags=pygame.NOFRAME
 pygame.display.set_caption("Pygame itProger Game")
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 
-square = pygame.Surface((50, 170))
-square.fill("Blue")
+bg = pygame.image.load("images/bg.png")
+# player = pygame.image.load("images/idle.png")
+walk_left = [
+    pygame.image.load("images/player_left/player_left1.png"),
+    pygame.image.load("images/player_left/player_left2.png"),
+    pygame.image.load("images/player_left/player_left3.png"),
+    pygame.image.load("images/player_left/player_left4.png"),
+    pygame.image.load("images/player_left/player_left5.png"),
+    pygame.image.load("images/player_left/player_left6.png")
+]
+walk_right = [
+    pygame.image.load("images/player_right/player_right1.png"),
+    pygame.image.load("images/player_right/player_right2.png"),
+    pygame.image.load("images/player_right/player_right3.png"),
+    pygame.image.load("images/player_right/player_right4.png"),
+    pygame.image.load("images/player_right/player_right5.png"),
+    pygame.image.load("images/player_right/player_right6.png")
+]
 
-myfont = pygame.font.Font("fonts/Roboto-Black.ttf", 40)
-text_surface = myfont.render("itProger", True, "Red")
+player_anim_count = 0
+bg_x = 0
 
-player = pygame.image.load("images/icon.png")
+bg_sound = pygame.mixer.Sound("sounds/bg.mp3")
+bg_sound.play()
 
 running = True
 while running:
 
-    pygame.draw.circle(square, "Red", (10, 7), 5)
-    screen.blit(square, (10, 0))
-    screen.blit(text_surface, (300, 100))
-    screen.blit(player, (100, 50))
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + 1280, 0))
+    screen.blit(walk_right[player_anim_count], (640, 550))
+
+    if player_anim_count == 5:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
+    
+    bg_x -= 2
+    if bg_x == -1280:
+        bg_x = 0
 
     pygame.display.update()
 
@@ -29,3 +56,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+    
+    clock.tick(20)
